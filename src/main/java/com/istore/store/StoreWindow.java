2e365cdbcd6bdd3ae8e253c1b3e097c61ea4dcdc
lucas <lucas.moreau@supinfo.com>
2024-02-19 16:59:06 +0100

    add back button

--- src/main/java/com/istore/store/StoreWindow.java
+++ src/main/java/com/istore/store/StoreWindow.java
@@ -7,17 +7,10 @@ import java.awt.*;
 public class  StoreWindow extends JPanel {
 
     private JLabel storeNameLabel;
-    private JButton backButton;
 
     public StoreWindow(Store store, StoreController storeController, WindowManager windowManager) {
         setLayout(new BorderLayout());
         storeNameLabel = new JLabel(store.getName());
         add(storeNameLabel, BorderLayout.NORTH);
-
-        backButton = new JButton("Retour");
-        add(backButton, BorderLayout.SOUTH);
-
-        backButton.addActionListener(e -> windowManager.goToWindow(new ListStoreWindow(storeController, windowManager)));
-
     }
 }
