78f6d3d398c8b966800c93b67e11c2956f5ea92e
33699 <thimeo.ballesta@supinfo.com>
2024-02-20 10:13:06 +0100

    blabla

--- src/main/java/com/istore/Main.java
+++ src/main/java/com/istore/Main.java
@@ -26,13 +26,13 @@ public class Main {
         WhitelistUserModel whitelistUserModel = new WhitelistUserModel();
         WhitelistUserController whitelistUserController = new WhitelistUserController(whitelistUserModel);
 
-        UserModel userModel = new UserModel();
+        UserModel userModel = new UserModel(dbTools);
         userController = new UserController(userModel, whitelistUserController);
 
         // Temporary data
         // whitelistUserController.addWhitelistedEmail("bbb@bbb.fr");
-        userModel.addUser(new User("aaa@aaa.fr", "aaa", BCrypt.hashpw("aaa", BCrypt.gensalt()), Role.USER));
-        userModel.addUser(new User("ccc@ccc.fr", "ccc", BCrypt.hashpw("ccc", BCrypt.gensalt()), Role.ADMIN));
+//        userModel.addUser(new User("aaa@aaa.fr", "aaa", BCrypt.hashpw("aaa", BCrypt.gensalt()), Role.USER));
+//        userModel.addUser(new User("ccc@ccc.fr", "ccc", BCrypt.hashpw("ccc", BCrypt.gensalt()), Role.ADMIN));
 
         StoreModel storeModel = new StoreModel();
         storeModel.addStore(new Store("Magasin 1", "1"));
